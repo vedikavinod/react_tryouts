e1e49f85d767c39dafcd8ad49081ed4f403c4cab
Vedika Vinod <vedikavinod74@gmail.com>
2025-12-10 05:51:29 +0530

    removed the useless comment

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
 
   const [users, setUsers] = useState(data);
 
-  // adding a useless comment here
   const deleteItem = (index) => {
     console.log("users before", users);
     const deletedarray = [...users.slice(0, index), ...users.slice(index + 1)];
